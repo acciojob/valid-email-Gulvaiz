e753abe5cf526e1814e0cdce33cc17f4a5e85433
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-31 12:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function validEmail(str) {
   //your JS code here.
+  valid = true
+   let indeAt = str.indexOf("@")
+   if(indeAt == 0){
+       valid = false
+       return valid
+   }
+   
+   if(str == ""){
+     valid = false
+     return valid
+   }
+   
+   else{
+       for(let i=0; i<indeAt; i++){
+            if(str[i] === "."){
+              valid = false
+            }
+            return valid
+       }
+   }
+   
+   return valid
 }
-
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
